550cb030ecd28e0c98d30f4f18102857f0b494a3
UAHollow <uahollow@gmail.com>
2018-05-07 13:58:45 +0300

    added proper inputs.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,7 @@
 package com.company;
- import java.util.Comparator;
- import java.util.Scanner;
- import java.util.Arrays;
+ import java.util.*;
 
- public class Main {
+public class Main {
      public static class customComparator implements Comparator{
          @Override
          public int compare(Object o1, Object o2) {
@@ -11,10 +9,16 @@ package com.company;
          }
      }
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        List<String> inputs = new LinkedList<>();
+        while (scanner.hasNextLine()) {
+            inputs.add(scanner.nextLine().trim());
+        }
         customComparator comp = new customComparator();
-        Arrays.sort(args, comp);
-        for(String i:args) {
-            System.out.println(i);
+        inputs.sort(comp);
+        for(String words:inputs) {
+            System.out.println(words);
         }
     }
 
